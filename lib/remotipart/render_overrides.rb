a58ca78c622757a009ddfd773b031a7aaf06ab64
Mikhail Nikalyukin <idups1k@gmail.com>
2016-08-17 12:13:14 +0300

    Update render_overrides.rb

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -14,7 +14,7 @@ module Remotipart
       render_without_remotipart *args
       if remotipart_submitted?
         textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
-        response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script>#{textarea_body}}
+        response.body = #{textarea_body}
         response.content_type = Mime::HTML
       end
       response_body
